b11175c9fbc8838ba559e659611f1659b5131e2c
Akash Gupta <gr8akash1994@gmail.com>
2024-03-03 22:14:56 +0530

    kafka

---
+++ deliveryboy/src/main/java/com/example/demo/config/AppConstants.java
@@ -0,0 +1,10 @@
+package com.example.demo.config;
+
+//Har jagah ye constant use kr lenge for topic name
+
+
+public class AppConstants {
+	
+	public static final String LOCATION_TOPIC_NAME="location-update-topic";
+
+}
---
+++ enduser/src/main/java/com/example/demo/config/KafkaConsumerConfig.java
@@ -0,0 +1,16 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.KafkaListener;
+
+import com.example.demo.constants.AppConstants;
+
+@Configuration
+public class KafkaConsumerConfig {
+
+	@KafkaListener(topics=AppConstants.LOCATION_UPDATE_TOPIC, groupId=AppConstants.GROUP_ID)
+	public void updatedLocation(String value)
+	{
+		System.out.print(value);
+	}
+}
